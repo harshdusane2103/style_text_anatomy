f4eaab174f53029ca372e641b120e2c7af1697b5
Harsh Dusane <harshdusane5@gmail.com>
2024-03-18 22:11:18 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/style_text_anatomy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.style_text_anatomy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          leading: Icon(Icons.menu, color: Colors.black),
+          title: Text(
+            'Flutter App',
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: Text.rich(
+            TextSpan(children: [
+              TextSpan(
+                  text: 'Red & White ',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 48,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.yellow,
+                  )),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Text('Red & White ',style: TextStyle(color: Colors.red,fontWeight: FontWeight.w700,fontSize:48,
+// decoration:TextDecoration.underline,
+// decorationColor: Colors.yellow ,
+//
+// )
+// ),
